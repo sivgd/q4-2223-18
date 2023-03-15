7d13709d92e7f3c637d76f34b29ccc72465d4043
VGD_2022x23_John_Fotheringham <1713323@mukilteo.wednet.edu>
2023-03-15 10:23:18 -0700

    Started on combat trigger collision connection

--- q4-2223-18/Assets/Enemy/Scripts/EnterCombatTrigger.cs
+++ q4-2223-18/Assets/Enemy/Scripts/EnterCombatTrigger.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class EnterCombatTrigger : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
-    }
+        if (collision.CompareTag("Player"))
+        {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        }
     }
 }
